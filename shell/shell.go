c30af2850de707f624b322d6edf6d7318d4ec059
Lee Keitel <lee@keitel.xyz>
2017-07-09 14:52:18 -0500

    Shell.NoExec(), panic recover to revert terminal

--- shell/shell.go
+++ shell/shell.go
@@ -8,21 +8,27 @@ import (
 	"github.com/lfkeitel/lish/exec"
 )
 
+const (
+	defaultPrompt = "$ "
+)
+
 type Terminal interface {
 	EnableRawMode() error
 	DisableRawMode() error
 	ReadLine(prompt string) (string, error)
 	ReadPassword(prompt string) (string, error)
-	Println(a ...interface{})
+	Println(a ...interface{}) (int, error)
+	AddHistory(item string)
 }
 
 type Shell struct {
-	env      env.Environment
+	env      *env.Environment
 	terminal Terminal
 	pwd      string
+	exec     bool
 }
 
-func New(environ env.Environment, term Terminal) *Shell {
+func New(environ *env.Environment, term Terminal) *Shell {
 	if environ == nil {
 		environ = env.New()
 	}
@@ -36,16 +42,28 @@ func New(environ env.Environment, term Terminal) *Shell {
 		env:      environ,
 		terminal: term,
 		pwd:      pwd,
+		exec:     true,
 	}
 }
 
+func (s *Shell) NoExec() {
+	s.exec = false
+}
+
 func (s *Shell) Run() error {
+	defer func() {
+		if err := recover(); err != nil {
+			s.terminal.DisableRawMode()
+			s.terminal.Println(err)
+		}
+	}()
+
 	if err := s.terminal.EnableRawMode(); err != nil {
 		return err
 	}
 
 	for {
-		line, err := s.terminal.ReadLine(">> ")
+		line, err := s.terminal.ReadLine(s.env.GetDefault("PS1", defaultPrompt))
 		if err != nil {
 			return err
 		}
@@ -54,6 +72,8 @@ func (s *Shell) Run() error {
 		if len(args) == 0 {
 			continue
 		}
+
+		s.terminal.AddHistory(line)
 
 		if args[0] == "cd" {
 			path, _ := filepath.Abs(args[1])
@@ -71,9 +91,11 @@ func (s *Shell) Run() error {
 			break
 		}
 
-		s.terminal.DisableRawMode()
-		err = exec.Run(args[0], args[1:], s.env, s.pwd)
-		s.terminal.EnableRawMode()
+		if s.exec {
+			s.terminal.DisableRawMode()
+			err = exec.Run(args[0], args[1:], s.env, s.pwd)
+			s.terminal.EnableRawMode()
+		}
 
 		if err != nil {
 			s.terminal.Println(err.Error())
