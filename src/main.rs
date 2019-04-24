1087dd81d18588d67054fe5d7b84616c31c5b1d7
Lee Keitel <lee@keitel.xyz>
2019-04-24 15:24:46 -0500

    Implemented free form commands and "call"
    The "call" function invokes a system command with
    given arguments. It can be used explicitly as a
    function or implicitly when calling a system
    command that doesn't exist as a function in Lazuli.
    
    Example:
    
    (call ls -lah)
    (ls -lah)
    
    The above two commands will do the same thing so long
    as there's no defined function named "ls" in the current
    scope. Functions defined in the shell vm override external
    commands.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use terminal::Terminal;
 use lazuli_vm::compiler::lexer::{ByteIter, Lexer};
 use lazuli_vm::compiler::parser::Parser;
 use lazuli_vm::object::cons_list::ConsList;
-use lazuli_vm::object::{Node, Symbol};
+use lazuli_vm::object::{Callable, Node, Symbol};
 use lazuli_vm::vm::VM;
 use lazuli_vm::{args_setup, args_setup_error};
+
 use path_absolutize::*;
+use std::collections::HashMap;
 use std::env;
 use std::path;
+use std::process::Command;
 
 const PROMPT: &str = "lish$ ";
 
@@ -21,6 +24,9 @@ fn main() {
     vm.add_symbol(Symbol::new_with_builtin("exit", shell_exit).into_ref());
     vm.add_symbol(Symbol::new_with_builtin("pwd", shell_pwd).into_ref());
     vm.add_symbol(Symbol::new_with_builtin("cd", shell_cd).into_ref());
+    vm.add_symbol(Symbol::new_with_builtin("call", shell_call).into_ref());
+
+    vm.set_cmd_not_found(Callable::Builtin(shell_call));
 
     loop {
         let mut line = term.readline(&PROMPT);
@@ -40,8 +46,9 @@ fn main() {
         };
 
         // println!("{:?}", tree);
-        if let Err(e) = vm.run(&tree) {
-            println!("{}", e);
+        match vm.run(&tree) {
+            Ok(v) => println!("{}", v),
+            Err(e) => println!("{}", e),
         }
     }
 }
@@ -88,4 +95,42 @@ fn shell_cd(vmc: &mut VM, args: ConsList<Node>) -> Result<Node, String> {
         Ok(_) => Ok(Node::empty_list()),
         Err(e) => Err(format!("{}", e)),
     }
+}
+
+fn shell_call(vm: &mut VM, args: ConsList<Node>) -> Result<Node, String> {
+    let args = args_setup!(args, "call", >=, 1);
+
+    let mut cmd = Command::new(format!("{}", vm.eval(&args[0])?));
+
+    for arg in args.iter().skip(1) {
+        cmd.arg(format!("{}", vm.eval(arg)?));
+    }
+
+    let res = cmd.output();
+
+    let mut map = HashMap::new();
+
+    match res {
+        Ok(out) => {
+            map.insert(
+                ":stdout".to_owned(),
+                Node::String(String::from_utf8(out.stdout).unwrap_or_default()),
+            );
+            map.insert(
+                ":stderr".to_owned(),
+                Node::String(String::from_utf8(out.stderr).unwrap_or_default()),
+            );
+            map.insert(
+                ":status".to_owned(),
+                Node::Number(i64::from(out.status.code().unwrap_or(255))),
+            );
+        }
+        Err(e) => {
+            map.insert(":stdout".to_owned(), Node::String("".to_owned()));
+            map.insert(":stderr".to_owned(), Node::String(format!("{}", e)));
+            map.insert(":status".to_owned(), Node::Number(255));
+        }
+    }
+
+    Ok(Node::from_hashmap(map))
 }
